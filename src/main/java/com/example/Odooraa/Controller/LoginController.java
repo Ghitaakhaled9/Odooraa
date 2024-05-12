d62fa94e26fd9b535d67c2d655cd689d8c806c3e
ghita <ghitakhaled14@gmail.com>
2024-05-12 18:17:22 +0100

    profil

--- src/main/java/com/example/Odooraa/Controller/LoginController.java
+++ src/main/java/com/example/Odooraa/Controller/LoginController.java
@@ -3,13 +3,14 @@ package com.example.Odooraa.Controller;
 import com.example.Odooraa.Repository.UserRepository;
 import com.example.Odooraa.entities.UserSite;
 import com.example.Odooraa.entities.UserType;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class LoginController {
@@ -24,22 +25,24 @@ public class LoginController {
     @PostMapping("/login/sucess")
     public String login(@RequestParam("email") String email,
                         @RequestParam("password") String password,
+                        HttpSession session,
                         RedirectAttributes redirectAttributes) {
 
         // Retrieve user by email from the database
         UserSite user = userRepository.findByEmail(email);
-        System.out.println(user.getType());
+
         // Check if user exists and password matches
         if (user != null && user.getPassword().equals(password)) {
-            // Check if the user is an admin
+            // Store user information in session
+            session.setAttribute("user", user);
+
+            // Check user type and redirect accordingly
             if (user.getType() == UserType.ADMIN) {
                 // Redirect to admin dashboard
                 return "redirect:/dashbord";
-            } else if (user.getType() == UserType.GERANT){
-
+            } else if (user.getType() == UserType.GERANT) {
                 return "redirect:/index";
-            }
-            else {
+            } else {
                 return "redirect:/index";
             }
         } else {
@@ -48,7 +51,4 @@ public class LoginController {
             return "redirect:/login";
         }
     }
-
-
-
 }
