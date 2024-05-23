fa47bffc925359ab4f4c11bc1758ef47353c1719
kaoutar_sarsari <kaoutarsarsari@gmail.com>
2024-05-23 12:51:19 +0100

    fix the error on the single.html page

--- src/main/java/com/example/Odooraa/Repository/ProduitRepository.java
+++ src/main/java/com/example/Odooraa/Repository/ProduitRepository.java
@@ -4,4 +4,5 @@ import com.example.Odooraa.entities.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
+    
 }
--- src/main/java/com/example/Odooraa/entities/Produit.java
+++ src/main/java/com/example/Odooraa/entities/Produit.java
@@ -14,6 +14,7 @@ public class Produit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name ;
     private String imageUrl; // add attribute for image URL
     private String marque;
     private String categorie;
